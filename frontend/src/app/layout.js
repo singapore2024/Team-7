4a00062a0b2c3e31d5f21759519d85b48621b088
vivienherq <heruoqingvivien@gmail.com>
2024-10-11 15:06:14 +0800

    combine all pages

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,8 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 
-import TopNavBar from "@/components/TopNavBar";
-
+import TopNavBar from "../components/TopNavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
