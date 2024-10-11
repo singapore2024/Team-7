164d1499deebd14ebdffab75ca852fbfb596aa64
vivienherq <heruoqingvivien@gmail.com>
2024-10-11 11:26:57 +0800

    add navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextUIProvider>
+          <NavBar />
+          <main className="container mx-auto px-4">{children}</main>
+        </NextUIProvider>
       </body>
     </html>
   );
---
+++ src/app/market/page.js
@@ -0,0 +1,35 @@
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+
+const Market = () => {
+  return (
+    <div>
+      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+        {list.map((item, index) => (
+          <Card
+            shadow="sm"
+            key={index}
+            isPressable
+            onPress={() => console.log("item pressed")}
+          >
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={item.title}
+                className="w-full object-cover h-[140px]"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-500">{item.price}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Market;
